45b68bfc0c9b69fcd46f2537f17d25cbf9bb5965
MAL1S <MAL1S@users.noreply.github.com>
2020-11-22 20:35:24 +0800

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nasledovanie
+{
+    public partial class Form1 : Form
+    {
+        List<Gadget> gadgetList = new List<Gadget>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //refillButton.PerformClick();
+            ShowInfo();
+        }
+
+        private void refillButton_Click(object sender, EventArgs e)
+        {
+            gadgetList.Clear();
+            outputBox.Clear();
+            Random rnd = new Random();
+            for(var i = 0; i < 10; i++)
+            {
+                switch (rnd.Next() % 3)
+                {
+                    case 0:                       
+                        gadgetList.Add(Notebook.generate());
+                        break;
+                    case 1:
+                        gadgetList.Add(Smartphone.generate());
+                        break;
+                    case 2:
+                        gadgetList.Add(Tablet.generate());
+                        break;
+                }
+                outputBox.Items.Add(gadgetList[i].ToString());
+            }
+            label1.Text = "->";
+            ShowInfo();
+        }
+
+        private void ShowInfo()
+        {
+            int notebookCount = 0;
+            int smartphoneCount = 0;
+            int tabletCount = 0;
+
+            foreach (var gadget in gadgetList)
+            {
+                if (gadget is Notebook)
+                {
+                    notebookCount++;
+                }
+                else if (gadget is Smartphone)
+                {
+                    smartphoneCount++;
+                }
+                else if (gadget is Tablet)
+                {
+                    tabletCount++;
+                }
+            }
+
+            
+            notebookCountLabel.Text = notebookCount.ToString();
+            smartphoneCountLabel.Text = smartphoneCount.ToString();
+            tabletCountLabel.Text = tabletCount.ToString();
+        }
+
+        private void getButton_Click(object sender, EventArgs e)
+        {
+            if (gadgetList.Count == 0)
+            {
+                txtOut.Text = "Пусто 0_о";
+                return;
+            }
+            else if (gadgetList.Count == 1) label1.Text = "";
+
+            var gadget = gadgetList[0];
+            gadgetList.RemoveAt(0);
+
+            txtOut.Text = gadget.getInfo();
+            outputBox.Items.RemoveAt(0);
+            ShowInfo();
+        }
+    }
+}
